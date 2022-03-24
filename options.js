16352c5edcca0da9b0d0da660204c90ec8a4104c
tuziben <tuziben@gmail.com>
2022-03-24 10:16:53 +0800

    for my ss proxy

--- options.js
+++ options.js
@@ -13,27 +13,6 @@ chrome.storage.local.get('socks5server', s => {
     s = s.socks5server || '';
     document.querySelector('#socks5server').value = s;
 });
-chrome.storage.local.get('bypassswitch', s =>{
-    s = s.bypassswitch || 'on';
-    if(s == "on"){
-        document.querySelector('#bypassswitch').checked = true;
-    }
-    if(s == "off"){
-        document.querySelector('#bypassswitch').checked = false;
-    }
-});
-chrome.storage.local.get('bypassdomainurl', s =>{
-    s = s.bypassdomainurl || 'https://txthinking.github.io/bypass/chinadomain.txt';
-    document.querySelector('#bypassdomainurl').value = s;
-});
-chrome.storage.local.get('bypasscidr4url', s =>{
-    s = s.bypasscidr4url || 'https://txthinking.github.io/bypass/chinacidr4.txt';
-    document.querySelector('#bypasscidr4url').value = s;
-});
-chrome.storage.local.get('bypasscidr6url', s =>{
-    s = s.bypasscidr6url || 'https://txthinking.github.io/bypass/chinacidr6.txt';
-    document.querySelector('#bypasscidr6url').value = s;
-});
 
 document.querySelector('#save').addEventListener("click", async (e) => {
     document.querySelector('#save').style.display = 'none';
@@ -41,10 +20,6 @@ document.querySelector('#save').addEventListener("click", async (e) => {
 
     var socks5switch = document.querySelector('#socks5switch').checked;
     var socks5server = document.querySelector('#socks5server').value;
-    var bypassswitch = document.querySelector('#bypassswitch').checked;
-    var bypassdomainurl = document.querySelector('#bypassdomainurl').value;
-    var bypasscidr4url = document.querySelector('#bypasscidr4url').value;
-    var bypasscidr6url = document.querySelector('#bypasscidr6url').value;
 
     if(socks5switch){
         if(!/.+:\d+/.test(socks5server)){
@@ -67,64 +42,10 @@ document.querySelector('#save').addEventListener("click", async (e) => {
         "<localhost>",
         "*.local",
 	];
-    if(bypassswitch){
-        try{
-            var r = await fetch(bypassdomainurl);
-            if(r.status != 200){
-                throw Error(`When fetch bypass list: ${r.status}`);
-            }
-            var s = await r.text();
-            var l1 = s.trim().split('\n');
-            l1.forEach(v=>{
-                l.push(v.trim());
-                l.push("*."+v.trim());
-            });
-            var r = await fetch(bypasscidr4url);
-            if(r.status != 200){
-                throw Error(`When fetch bypass list: ${r.status}`);
-            }
-            var s = await r.text();
-            var l1 = s.trim().split('\n');
-            l1.forEach(v=>{
-                l.push(v.trim());
-            });
-            var r = await fetch(bypasscidr6url);
-            if(r.status != 200){
-                throw Error(`When fetch bypass list: ${r.status}`);
-            }
-            var s = await r.text();
-            var l1 = s.trim().split('\n');
-            l1.forEach(v=>{
-                l.push(v.trim());
-            });
-        }catch(e){
-            alert(`When fetch bypass list: ${e.message}`);
-            document.querySelector('#save').style.display = 'block';
-            document.querySelector('#ing').style.display = 'none';
-            return;
-        }
-    }
-    chrome.storage.local.set({"bypassswitch": bypassswitch ? 'on' : 'off'});
-    chrome.storage.local.set({"bypassdomainurl": bypassdomainurl});
-    chrome.storage.local.set({"bypasscidr4url": bypasscidr4url});
-    chrome.storage.local.set({"bypasscidr6url": bypasscidr6url});
-
-    if(!socks5switch){
-        chrome.proxy.settings.set({
-            value: {
-                mode: "system",
-            },
-        },()=>{
-            setTimeout(()=>{
-                document.querySelector('#save').style.display = 'block';
-                document.querySelector('#ing').style.display = 'none';
-            }, 2000);
-        });
-        return;
-    }
 
     var host = socks5server.substring(0, socks5server.lastIndexOf(':')).replace('[', '').replace(']', '');
     var port = socks5server.substring(socks5server.lastIndexOf(':')+1);
+
     chrome.proxy.settings.set({
         value: {
             mode: "fixed_servers",
